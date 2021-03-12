ac71262397fa5ca351f900df52689150246a304b
Nikita Ratnikov <nikkie@evrone.com>
2021-03-12 15:15:20 +0300

    initial commit

---
+++ spec/pinger_ping_check_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe PingerPingCheck do
+  it "has a version number" do
+    expect(PingerPingCheck::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
